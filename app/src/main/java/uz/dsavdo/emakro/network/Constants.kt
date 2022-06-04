d7f7894067d2d85d1302edbe686a25024398cbf4
islomyuldashev <islomyuldashev3007@gmail.com>
2022-06-04 16:45:51 +0500

    Update

--- app/src/main/java/uz/dsavdo/emakro/network/Constants.kt
+++ app/src/main/java/uz/dsavdo/emakro/network/Constants.kt
@@ -2,8 +2,8 @@ package uz.dsavdo.emakro.network
 
 class Constants {
     companion object{
-        const val BASE_URL = "https://makromarket.uz/api/"
-        const val CLIENT_BASE_URL = "https://botf.makromarket.uz/api/client/"
+        const val BASE_URL = ""
+        const val CLIENT_BASE_URL = ""
         const val LOGIN_ON = 1001
         const val LOGIN_OFF = 1000
     }
